a986cc8003a73a4f446ebba7927e3a4483e5ab05
Bluskyo <blusky445@gmail.com>
2025-05-07 17:34:47 +0200

    Added json formatting and files for db

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import json
 
 def makeCSV():
-    with open("data/allWords/words.csv", "w") as writeFile:
+    with open("data/allWords/JLPTWords.csv", "w") as writeFile:
         writeFile.write(f"Word,JLPTLevel\n")
         for jlptLevel in range(1, 6):
             with open(f"data/rawData/N{jlptLevel}.txt") as file:
                 for line in file:
                     writeFile.write(f"{line.strip()},N{jlptLevel}\n")
-        
-makeCSV()
+
+def makeJSON():
+    dictonary = {}
+
+    for jlptLevel in range(1, 6):
+        with open(f"data/rawData/N{jlptLevel}.txt") as file:
+            for line in file:
+                dictonary[line.strip()] = f"N{jlptLevel}"
+    
+    dictonaryJson = json.dumps(dictonary, indent=4, ensure_ascii=False)
+
+    print(dictonaryJson)
+
+    with open("data/allWords/JLPTWords.json", "w", encoding='utf8') as writeFile:
+        writeFile.write(dictonaryJson)
+
+##makeCSV()
+##makeJSON()
